f6988293de4a0f6f460a7626bcea2485458bcbb0
Karena Abhay <abhaykarena11@gmail.com>
2025-07-08 01:49:57 +0530

    final

--- Backend/socketIO/server.js
+++ Backend/socketIO/server.js
@@ -1,39 +1,39 @@
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server ,{
-    cors:{
-        origin : "http://localhost:5173",
-        
-        methods: ["GET" , "POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "https://chat-6iyh.onrender.com",
+
+    methods: ["GET", "POST"],
+  },
 });
 
-const users= {};
+const users = {};
 
-io.on("connection",(socket)=>{
-    console.log("new client connection  "+ socket.id);
+io.on("connection", (socket) => {
+  console.log("new client connection  " + socket.id);
 
-    const userId = socket.handshake.query.userId;
-    if(userId){
-        users[userId] = socket.id;
-    }
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    users[userId] = socket.id;
+  }
 
+  io.emit("getOnline", Object.keys(users));
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected" + socket.id);
+    delete users[userId];
     io.emit("getOnline", Object.keys(users));
-
-    socket.on("disconnect", ()=>{
-        console.log("Client disconnected"+socket.id);
-        delete users[userId];
-        io.emit("getOnline", Object.keys(users));
-    })
+  });
 });
 
 const getSocketId = (userId) => {
-    return users[userId];
-}
+  return users[userId];
+};
 
-export {app,io,server,getSocketId};
+export { app, io, server, getSocketId };
